99d7aa0b68adbc79959079937dc0a2da8ca685a4
TanHaven <2582003996@qq.com>
2022-06-18 17:38:00 +0800

    AllOk

--- pages/message/message.vue
+++ pages/message/message.vue
@@ -1,6 +1,20 @@
 <template>
-	<view>
-		<text selectable="true">这是私信页面</text>
+	<view class="message">
+		<view class="top flex">
+			<view class="left flex justify-start">
+				<input class="text1" disabled="true" @click="" value="好友"/>
+				<input class="text2" disabled="true" @click="" value="搜索"/>
+			</view>
+			<view class="right text-xl text-blue">
+				<input class="more cuIcon-roundaddfill" @click=""/>
+			</view>
+		</view>
+		
+		<view class="content">
+			<slot>
+				
+			</slot>
+		</view>
 	</view>
 </template>
 
@@ -17,5 +31,37 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	@import "../../steam.scss";
+	page{
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		background-color: $steam-background-color;
+	}
+	.message{
+		.top{
+			margin-top: 10%;
+			margin-left: 100rpx;
+			.left{
+				.text1{
+					width: 50rpx;
+					
+				}
+				.text2{
+					margin-left: 50rpx;
+					width: 50rpx;
+				}
+			}
+			.right{
+				margin-left: 60%;
+				.more{
+					width: 100rpx;
+					height: 100rpx;
+				}
+			}
+		}
+	}
+	
 </style>
